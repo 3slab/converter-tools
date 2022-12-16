d4272a934583e23be03e3f2da0b9ccf44949cd7e
Thomas Lecarpentier <thomas.lecarpentier@suez.com>
2022-12-16 14:27:52 +0100

    feat(converter) first commit

---
+++ src/SuezSmartSolution/ConverterTools/AnyBaseConverter.php
@@ -0,0 +1,160 @@
+<?php
+
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.suezsmartsolutions.com>.
+ */
+
+namespace SuezSmartSolution\ConverterTools;
+
+/**
+ * based and convert from 'any-base' javascript library (https://www.npmjs.com/package/any-base).
+ * @see https://github.com/HarasimowiczKamil/any-base/blob/7595a7398959618a93cb4edef0bae3e035eb3ce0/index.js
+ */
+class AnyBaseConverter implements ConverterInterface
+{
+
+    const BIN = '01';
+    const OCT = '01234567';
+    const DEC = '0123456789';
+    const HEX = '0123456789abcdef';
+
+
+    private string $srcAlphabet;
+    private string $dstAlphabet;
+
+    private string $invalidList='';
+
+    /**
+     * create object with joice of source base and destination base
+     */
+    public function __construct(string $srcAlphabet, string $dstAlphabet)
+    {
+
+        if (empty($srcAlphabet) || empty($dstAlphabet)) {
+            throw new \Exception('Bad alphabet');
+        }
+
+        if(!$this->isUniqueChar($srcAlphabet))
+        {
+            throw new \Exception('Chars in $srcAlphabet are not unique');
+        }
+        if(!$this->isUniqueChar($dstAlphabet))
+        {
+            throw new \Exception('Chars in $dstAlphabet are not unique');
+        }
+
+        $this->srcAlphabet = $srcAlphabet;
+        $this->dstAlphabet = $dstAlphabet;
+    }
+
+    /**
+    * 
+    * Implement an algorithm to determine if a string has all unique characters. 
+    * What if you can not use additional data structures?
+    * @see https://github.com/hrishikesh-mishra/algorithm/blob/82c389cf43b25e6547c49090f67c386fa68a4353/unique-character-in-string.php
+    * 
+    */
+    protected function isUniqueChar ($str) { 
+    
+        assert(is_string($str)) ; 
+        $unqiueChar  = array () ; 
+        $len = strlen($str) ; 
+        
+        for($i = 0 ; $i < $len ;  $i++) { 
+            $char = $str[$i];
+            if(in_array($char, $unqiueChar)) { 
+                return false;
+            }else { 
+                $unqiueChar [$char] = $char; 
+            }
+        }
+        
+        return true;
+        
+    }
+
+
+    /**
+     * convert number from source base to destination base
+     */
+    public function convert ($number) {
+        
+        $i=null;
+        $divide=null;
+        $newlen=null;
+        $numberMap = [];
+        $fromBase = strlen($this->srcAlphabet);
+        $toBase = strlen($this->dstAlphabet);
+        $length = strlen($number);
+        $result =  '';
+    
+        if (!$this->isValid($number)) {
+            throw new \Exception('Number "' . $number . '" contains "'.$this->invalidList.'" not included in digits (' . $this->srcAlphabet . ')');
+        }
+    
+        if ($this->srcAlphabet === $this->dstAlphabet) {
+            return $number;
+        }
+    
+        for ($i = 0; $i < $length; $i++) {
+            $numberMap[$i] = strpos($this->srcAlphabet,str_split($number)[$i]);
+        }
+   
+        do {
+            $divide = 0;
+            $newlen = 0;
+            for ($i = 0; $i < $length; $i++) {
+                $divide = $divide * $fromBase + $numberMap[$i];
+                if ($divide >= $toBase) {
+                    $numberMap[$newlen++] = intval($divide / $toBase, 10);
+                    $divide = $divide % $toBase;
+                } else if ($newlen > 0) {
+                    $numberMap[$newlen++] = 0;
+                }
+            }
+            $length = $newlen;
+
+            $result = substr($this->dstAlphabet,$divide, 1) . $result;
+            
+            
+        } while ($newlen !== 0);
+    
+
+
+        return $result;
+    }
+
+    /***
+     * validate alphabet list against input number
+     */
+    protected function isValid($number) {
+        $invalidChar = [];
+        $isValid = true;
+        $i = 0;
+        $numberLength = count(str_split($number));
+        for (; $i < $numberLength; ++$i) {
+            $testChar= str_split($number)[$i];
+            if (strpos($this->srcAlphabet,$testChar) === false) {
+                $invalidChar[$testChar] = $testChar;
+                $isValid = false;
+            }
+        }
+        $this->invalidList = implode('',$invalidChar);
+        return $isValid;
+    }
+
+}
---
+++ src/SuezSmartSolution/ConverterTools/ShortUUID.php
@@ -0,0 +1,196 @@
+<?php
+
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.suezsmartsolutions.com>.
+ */
+
+namespace SuezSmartSolution\ConverterTools;
+
+
+/**
+ * based and convert from 'short-uuid' javascript library (https://www.npmjs.com/package/short-uuid).
+ * @see https://github.com/oculus42/short-uuid/blob/bdd83c4a6cae19387796ec1e8fdf36129b819b50/index.js
+ */
+class ShortUUID
+{
+
+    const flickrBase58 = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+    const cookieBase90 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:<=>?@[]^_`{|}~";
+
+    /**
+     * used  alphabet
+     */
+    public $useAlphabet = ShortUUID::flickrBase58;
+
+    /**
+     * default Options
+     */
+    protected $defaultOptions = [
+        'consistentLength' => true // force length of result
+    ];
+
+    /**
+     * slected options. Merged from $this->defaultOptions and contructor $options
+     */
+    protected $selectedOptions = [];
+
+
+    public $maxLength; 
+
+    /**
+     * UUID shorten manipulator
+     * it's possible to change Converter with param $fromHexConverter and $toHexConverter. This converter need to implements ConverterInterface
+     */
+    public function __construct(
+        $targetAlphabet = null, 
+        $options = [],
+        ConverterInterface $fromHexConverter=null,
+        ConverterInterface $toHexConverter = null)
+    {
+
+        if ($targetAlphabet !== null) {
+            $this->useAlphabet = $targetAlphabet;
+        }
+
+        $this->selectedOptions = array_merge($this->defaultOptions, $options);
+
+        if($fromHexConverter !== null)
+        {
+            $this->fromHex = $fromHexConverter;
+        }
+        else
+        {
+            $this->fromHex = new AnyBaseConverter(AnyBaseConverter::HEX, $this->useAlphabet);
+        }
+
+        if($toHexConverter !== null)
+        {
+            $this->toHex = $toHexConverter;
+        }
+        else
+        {
+            $this->toHex = new AnyBaseConverter($this->useAlphabet, AnyBaseConverter::HEX);
+        }
+        
+        
+
+        $this->maxLength = $this->getShortIdLength(strlen($this->useAlphabet));
+
+        $this->paddingParams = [
+            'shortIdLength' => $this->maxLength,
+            'consistentLength' => $this->selectedOptions['consistentLength'],
+            'paddingChar' =>  substr($this->useAlphabet, 0, 1),
+        ];
+    }
+
+    /**
+     * internal shorten UUID function
+     */
+    protected function shortenUUID($longId,ConverterInterface $translator, $paddingParams)
+    {
+        $translated = $translator->convert(str_replace('-', '', strtolower($longId)));
+
+        if (!empty($paddingParams) || !$paddingParams['consistentLength']) {
+            return $translated;
+        }
+
+        return str_pad(
+            $translated,
+            $paddingParams['shortIdLength'],
+            $paddingParams['paddingChar'],
+            STR_PAD_LEFT
+        );
+    }
+
+    /**
+     * internal enlarge UUID function
+     */
+    protected function enlargeUUID($shortId,ConverterInterface $translator)
+    {
+        $uu1 = str_pad($translator->convert($shortId), 32, '0', STR_PAD_LEFT);
+        $m = [];
+        // Join the zero padding and the UUID and then slice it up with match
+        preg_match('/(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/', $uu1,$m);
+        // Accumulate the matches and join them.
+        return implode('-', [$m[1], $m[2], $m[3], $m[4], $m[5]]);
+    }
+
+
+    /**
+     * reduce UUID size based on enw base.
+     */
+    public function fromUUID($uuid)
+    {
+        return $this->shortenUUID($uuid, $this->fromHex, $this->paddingParams);
+    }
+
+    /**
+     * enlarge UUID previouly shorten by fromUUID
+     */
+    public function toUUID($shortUuid)
+    {
+        return $this->enlargeUUID($shortUuid, $this->toHex);
+    }
+
+
+    protected function getShortIdLength($alphabetLength)
+    {
+        return ceil(log(2 ** 128) / log($alphabetLength));
+    }
+
+    /**
+     * generate UUIDV4 id
+     */
+    public function generate()
+    {
+        return $this->shortenUUID($this->uuidv4(), $this->fromHex, $this->paddingParams);
+    }
+
+    /**
+     * alias  to generate
+     */
+    public function new()
+    {
+        return $this->generate();
+    }
+
+    /**
+     * alias  to guidv4
+     */
+    public function uuidv4()
+    {
+        return $this->guidv4();
+    }
+
+    /**
+     * @see https://www.uuidgenerator.net/dev-corner/php
+     */
+    public function guidv4($data = null)
+    {
+        // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+        $data = $data ?? random_bytes(16);
+        assert(strlen($data) == 16);
+
+        // Set version to 0100
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        // Set bits 6-7 to 10
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Output the 36 character UUID.
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+}
